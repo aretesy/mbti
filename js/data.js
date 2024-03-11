b5d5a8011eae1448b4c39fb9e857ba289bbb1e8d
aretesy <aretesy@naver.com>
2024-03-11 21:46:39 +0900

    색상 변경, data.js 내용 변경

--- js/data.js
+++ js/data.js
@@ -3,24 +3,24 @@ export const questions = [
     number: '01',
     question: '곧 크리스마스! 회사에서 크리스마스 파티 스텝을 모집한다는데?',
     choices: [
-      { text: '파티 스텝은 못하지ㅎ...<br>(그래도 파티는 재밌겠다 히히)', value: 'i' },
-      { text: '재밌겠다!<br>○○한테 같이 지원하자고 해야지~', value: 'e' }
+      { text: '파티 스텝은 못하지ㅎ...<br>(파티...?도 안가도 되나...)', value: 'i' },
+      { text: '재밌겠다!<br>소연이한테 같이 지원하자고 해야지~', value: 'e' }
     ]
   },
   {
     number: '02',
     question: '해외 출장이 잡혔다!<br>전날 잠들기 전 나는?',
     choices: [
-      { text: '짐 다 챙겼고 일찍 자야지~', value: 's' },
-      { text: '다 챙겼겠지?<br>여분 옷을 좀 더 챙길까..(뒤척뒤척)', value: 'n' }
+      { text: '짐 다 챙겼으니까 일찍 자야지~', value: 's' },
+      { text: '다 챙겼겠지?<br>옷을 좀 더 챙길까..(뒤적뒤적)', value: 'n' }
     ]
   },
   {
     number: '03',
     question: '친했던 동기가<br>퇴사를?',
     choices: [
-      { text: '일하면서 많이 힘들었나?<br>내가 좀 더 챙겨줄걸.', value: 'f' },
-      { text: '아 이직하나?<br>퇴사하고 뭐하냐고 물어봐야지.', value: 't' }
+      { text: '일이 많이 힘들었나?<br>내가 좀 더 챙겨줄걸.', value: 'f' },
+      { text: '이직하나?<br>퇴사하고 뭐하냐고 물어봐야지.', value: 't' }
     ]
   },
   {
@@ -28,14 +28,14 @@ export const questions = [
     question: '대규모 프로젝트를 맡게 되어 떨리는 마음에 처음 시작한 건?',
     choices: [
       { text: '큰 프로젝트인 만큼 계획이 중요해!<br>목적과 목표 먼저 정리해봐야지!', value: 'j' },
-      { text: '당장 미팅 잡고<br>팀원이랑 이야기하면 되겠지?', value: 'p' }
+      { text: '일단 시작해보자!<br>미팅 먼저 잡고 팀원이랑 이야기하면 되겠지?', value: 'p' }
     ]
   },
   {
     number: '05',
     question: '첫 입사 날!<br>나의 첫 인사말은?',
     choices: [
-      { text: '안녕하세요ㅎㅎ 잘 부탁드려요~<br>(부끄러워 >///<)', value: '' },
+      { text: '안녕하세요ㅎㅎ 잘 부탁드려요~<br>(아... 집에 가고 싶다.)', value: '' },
       { text: '안녕하세요! 저는 자취하고 있고요,<br>MBTI는 ~~ (빨리 친해져야지 >0<) ', value: '' }
     ]
   },
@@ -52,7 +52,7 @@ export const questions = [
     question: '상사한테 한 소리 들었다.<br>나는?',
     choices: [
       { text: '더 열심히 해서 잘할 수 있다는<br>사람이란 걸 보여주자!', value: '' },
-      { text: '내가 재능이 없나?<br>퇴사각..', value: '' }
+      { text: '내가 재능이 없나?<br>퇴사각...', value: '' }
     ]
   },
   {
@@ -68,15 +68,15 @@ export const questions = [
     question: '좋은 곳에서 이직 제안이 왔다!<br>나의 선택은?',
     choices: [
       { text: '인생은 도전이야!<br>좋은 기회라고 생각하고 시작해볼래!', value: '' },
-      { text: '지금 회사도 좋은데..<br>위험 부담을 안고 이직하는 게 맞을까?', value: '' }
+      { text: '지금 회사도 좋은데...<br>위험 부담을 안고 이직하는 게 맞을까?', value: '' }
     ]
   },
   {
     number: '10',
     question: '회사에서 진급 대상이 되었다.<br>나의 마음은?',
     choices: [
-      { text: '드디어 회사가 날 알아주는구나!<br>더 열심히 해야지!', value: '' },
-      { text: '진급하면 월급도 오르겠지?<br>월급 오르면 ○○ 사야지~ (상상의 나래~)', value: '' }
+      { text: '드디어 회사가 날 알아주는구나!<br>더 열심히 하자!', value: '' },
+      { text: '진급하면 월급도 오르겠지?<br>월급 오르면 뭐부터 사야하지~', value: '' }
     ]
   },
 ]
